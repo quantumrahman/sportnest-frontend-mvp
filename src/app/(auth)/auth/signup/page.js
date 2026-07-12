eede6df9305d10f04d0e80a79dca7b5aca2be710
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-13 01:24:28 +0600

    feat: add sign-up form and implement sign-up page with finalized design

--- src/app/(auth)/auth/signup/page.js
+++ src/app/(auth)/auth/signup/page.js
@@ -1,3 +1,57 @@
-export default function SignupPage() {
-    return <h1>Sign-up page</h1>;
+'use client';
+
+import SignUpForm from '@/components/ui/SignUpForm';
+import Link from 'next/link';
+import { FaGoogle } from 'react-icons/fa6';
+
+export default function SigninPage() {
+    return (
+        <div className="flex min-h-screen w-full items-center justify-center bg-neutral-900 px-5 md:px-10">
+            <div className="flex w-full max-w-[450px] flex-col items-center justify-center">
+                <div className="mb-5 w-full text-center">
+                    <Link href={'/'}>
+                        <h1 className="text-lg font-bold text-neutral-50 uppercase">
+                            SportNest
+                        </h1>
+                    </Link>
+                </div>
+                <div className="flex w-full flex-col items-center text-center">
+                    <h2 className="text-[32px] font-semibold text-neutral-50">
+                        Create account
+                    </h2>
+                    <p className="mt-2 w-full max-w-[400px] text-sm text-neutral-400">
+                        Create your SportNest account to book your favorite
+                        sports facilities anytime.
+                    </p>
+                </div>
+                <button className="mt-10 flex w-full cursor-pointer items-center justify-center gap-2 rounded-full border border-neutral-700 py-2.5 text-base font-medium text-neutral-50 transition-colors duration-200 ease-linear hover:bg-neutral-700/20">
+                    <FaGoogle />
+                    Sign in with Google
+                </button>
+                <div className="my-5 flex w-full items-center justify-center gap-2">
+                    <div className="h-px w-full bg-neutral-700"></div>
+                    <p className="text-sm font-medium text-neutral-500">or</p>
+                    <div className="h-px w-full bg-neutral-700"></div>
+                </div>
+                <SignUpForm />
+                <div className="mt-3 w-full text-center">
+                    <p className="text-center text-xs text-neutral-400">
+                        By continuing, you agree to SportNest{' '}
+                        <span className="cursor-pointer underline">Terms</span>{' '}
+                        of Service and{' '}
+                        <span className="cursor-pointer underline">
+                            Privacy Policy
+                        </span>
+                        .
+                    </p>
+                    <p className="mt-4 text-sm text-neutral-400">
+                        Already have an account?{' '}
+                        <Link href={'/auth/signin'} className="hover:underline">
+                            Sign In
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
---
+++ src/components/ui/SignUpForm.js
@@ -0,0 +1,56 @@
+'use client';
+
+import { FaExclamation } from 'react-icons/fa6';
+
+export default function SignUpForm() {
+    return (
+        <div className="w-full">
+            <form>
+                <div className="flex w-full items-center justify-center gap-4">
+                    <label htmlFor="name" className="w-full">
+                        <input
+                            type="text"
+                            id="name"
+                            placeholder="Full name"
+                            className="w-full rounded-xl border border-neutral-700 bg-neutral-700/20 px-4 py-[13px] text-white caret-white outline-none placeholder:text-neutral-500 focus:ring-2 focus:ring-white"
+                        />
+                        <span className="inline-flex h-4 w-full items-center text-sm text-red-500"></span>
+                    </label>
+                    <label htmlFor="image" className="w-full">
+                        <input
+                            type="text"
+                            id="image"
+                            placeholder="Photo"
+                            className="w-full rounded-xl border border-neutral-700 bg-neutral-700/20 px-4 py-[13px] text-white caret-white outline-none placeholder:text-neutral-500 focus:ring-2 focus:ring-white"
+                        />
+                        <span className="inline-flex h-4 w-full items-center text-sm text-red-500"></span>
+                    </label>
+                </div>
+                <label htmlFor="email" className="w-full">
+                    <input
+                        type="text"
+                        id="email"
+                        placeholder="Email address"
+                        className="w-full rounded-xl border border-neutral-700 bg-neutral-700/20 px-4 py-[13px] text-white caret-white outline-none placeholder:text-neutral-500 focus:ring-2 focus:ring-white"
+                    />
+                    <span className="inline-flex h-4 w-full items-center text-sm text-red-500"></span>
+                </label>
+                <label htmlFor="password" className="w-full">
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        className="w-full rounded-xl border border-neutral-700 bg-neutral-700/20 px-4 py-[13px] text-white caret-white outline-none placeholder:text-neutral-500 focus:ring-2 focus:ring-white"
+                    />
+                    <span className="inline-flex h-4 w-full items-center text-sm text-red-500"></span>
+                </label>
+                <button
+                    type="submit"
+                    className="hover flex w-full cursor-pointer items-center justify-center rounded-full bg-neutral-50 py-2.5 text-base font-medium text-neutral-950 transition-colors duration-200 ease-linear hover:bg-neutral-200"
+                >
+                    Continue
+                </button>
+            </form>
+        </div>
+    );
+}
