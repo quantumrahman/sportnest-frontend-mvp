09eecdb0c815d04a177fdf169c6d44a3ab60aeec
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-07-13 22:13:13 +0600

    feat: add react toast components

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,5 +1,11 @@
 import '../globals.css';
+import { Toaster } from 'react-hot-toast';
 
 export default function AuthLayout({ children }) {
-    return <main className="min-h-screen w-full">{children}</main>;
+    return (
+        <main className="min-h-screen w-full">
+            {children}
+            <Toaster />
+        </main>
+    );
 }
